c26862220ec9ce80a4445859b25838ddf0c36fb6
QuLink1040 <44401758+QuLink1040@users.noreply.github.com>
2022-06-08 20:32:49 +0800

    Initial commit

---
+++ Game Engine/Windowing System/window.h
@@ -0,0 +1,21 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#endif
+
+#include <window.h>
+#include <tchar.h>
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+);
+
+LRESULT CALLBACK WndProc(
+	_In_ HWND   hWnd,
+	_In_ UINT   message,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+);
+
